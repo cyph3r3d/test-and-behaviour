3341cdf4dd70a59727cc5e681f8dd46ad924186a
cyph3r3d <hillarykipngeno482@gmail.com>
2024-12-08 11:20:37 +0300

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -2,7 +2,7 @@
 # Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# you may not use this file except compliance with the License.
 # You may obtain a copy of the License at
 #
 # https://www.apache.org/licenses/LICENSE-2.0
